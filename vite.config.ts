7cebaaef7279f9d645d57521423ad9f18f13d7df
MissJoo <jonnavillaruz05@yahoo.com>
2026-01-13 16:54:21 +0300

    ffsdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+
+import path, { resolve } from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Miss_Joo_Writes/',
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger?.(),
+  ].filter(Boolean),
+
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
